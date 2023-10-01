4e8ab0679a20fa153148d77de00bc81c729ead0f
Raya Mahardika <rayapkun@gmail.com>
2023-10-01 21:59:10 +0700

    tissot: Do halcyon!

--- halcyon_tissot.mk
+++ halcyon_tissot.mk
@@ -8,16 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
-BOARD_VENDOR := Xiaomi
+PRODUCT_NAME := halcyon_tissot
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
